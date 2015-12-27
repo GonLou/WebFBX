4fee5aa5a183b8c027c082dbb9dec96cb9ae1f12
GonLou <gnunodal@hotmail.com>
2015-12-27 15:05:55 +0000

    put code in severall functions and zoom functionality in DisplayControlsPoints

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# WebFBX 
+# WebFBX
 
 http://nick.onetwenty.org/index.php/2010/04/07/creating-svg-files-with-python/
 http://tecfaetu.unige.ch/staf/staf-k/borer/stic3/SVG/TMs-SVG-Codebook.pdf
 https://www.safaribooksonline.com/library/view/programming-python-4th/9781449398712/ch01s08.html
+https://pypi.python.org/pypi/dominate
+https://en.wikipedia.org/wiki/Isometric_projection
--- index.py
+++ index.py
@@ -20,18 +20,17 @@ def SliceFileName(s1):
 # render to image
 # create an empty SVG XML element
 ########################################
-def DrawEmptySVG():
+def DrawEmptySVG(fileName):
     # create an empty SVG XML element
     doc = et.Element('svg', width='150', height='150', version='1.1', xmlns='http://www.w3.org/2000/svg')
     # add a circle (using the SubElement function)
     et.SubElement(doc, 'circle', cx='75', cy='75', r='70', fill='rgb(20, 255, 177)')
     # add text
     text = et.Element('text', x='75', y='75', fill='white', style='font-family:Courier;font-size:18px;text-anchor:middle;dominant-baseline:top')
-    print SVGline
     text.text = 'no image'
     doc.append(text)
     # ElementTree 1.2 doesn't write the SVG file header errata, so do that manually
-    f = open('thumbnails/sample.svg', 'w')
+    f = open('thumbnails/'+fileName+'.svg', 'w')
     f.write('<?xml version=\"1.0\" standalone=\"no\"?>\n')
     f.write('<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\"\n')
     f.write('\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n')
@@ -58,122 +57,116 @@ def DrawSVG(SVGline, fileName):
 def DisplayControlsPoints(pMesh):
     lControlPointsCount = pMesh.GetControlPointsCount()
     lControlPoints = pMesh.GetControlPoints()
-
-    print ("    Control Points")
+    zoom = 0.5
+    #print ("    Control Points")
     SVGline = ""
     for i in range(lControlPointsCount):
-        print("        Control Point %i" % i)
-        print("            Coordinates X: %s" % lControlPoints[i][0])
-        print("            Coordinates Y: %s" % lControlPoints[i][1])
-        print("            Coordinates Z: %s" % lControlPoints[i][2])
+        #print("        Control Point %i" % i)
+        #print("            Coordinates X: %s" % lControlPoints[i][0])
+        #print("            Coordinates Y: %s" % lControlPoints[i][1])
+        #print("            Coordinates Z: %s" % lControlPoints[i][2])
         if i < lControlPointsCount-1 :
             #SVGline = SVGline + str(lControlPoints[i][0]/lControlPoints[i][2]) + "," + str(lControlPoints[i][1]/lControlPoints[i][3]) + ","
             if lControlPoints[i][3] > 0:
-                SVGline = SVGline + str(lControlPoints[i][0]/lControlPoints[i][2]) + "," + str(lControlPoints[i][1]/lControlPoints[i][3]) + ","
+                SVGline = SVGline + str(lControlPoints[i][0]/lControlPoints[i][2]*zoom) + "," + str(lControlPoints[i][1]/lControlPoints[i][3]*zoom) + ","
             else:
-                SVGline = SVGline + str(lControlPoints[i][0]) + "," + str(lControlPoints[i][1]) + ","
+                SVGline = SVGline + str(lControlPoints[i][0]*zoom) + "," + str(lControlPoints[i][1]*zoom) + ","
 
     return SVGline[:-1]
-
-
-#temporary variables
-path = "C:\Python27\Scripts\WebFbx"
-#/C:/Python27/Scripts/WebFbx/thumbnails/sample.svg
-tmp_file_path = "files\cube.FBX";
-#tmp_file_path = "files\bat.fbx";
-#end temporary variables
-
-
-# get files from FILES directory
-found_files = glob.glob("files/*.fbx")
-print "FILES LIST: "
-for found_file in found_files:
-    print "    %s" % found_file
-    #tmp_file_path = found_file #process each file in FBX SDK
-
 
 ########################################
 # just doing one file to see how it goes, later will put it on function
 ########################################
+def ImportModel(filePath):
+    #creating instance (acess to C++ SDK) with a custom name
+    manager = fbx.FbxManager.Create()
+    importer = fbx.FbxImporter.Create(manager, "myImporter")
+    status = importer.Initialize(filePath)
 
+    #check if file is ok for load
+    if status == False:
+        print "WARNING: FbxImporter intialization failed"
+        DrawEmptySVG(SliceFileName(filePath))
+        return 0
+        #sys.exit()
 
-#creating instance (acess to C++ SDK) with a custom name
-manager = fbx.FbxManager.Create()
-importer = fbx.FbxImporter.Create(manager, "myImporter")
-status = importer.Initialize(tmp_file_path)
+    #import scene
+    scene = fbx.FbxScene.Create(manager, "myScene")
+    importer.Import(scene)
+    importer.Destroy()
+    print "model OK"
+    #extract first node
+    node = scene.GetRootNode()
+    #print("INFO: number of nodes = %i" % scene.GetNodeCount())
+    #print("INFO: number of geometries = %i" % scene.GetGeometryCount())
 
+    for i in range(node.GetChildCount()):
+        #print("INFO: number of child from node = %i" % node.GetChildCount())
+        child = node.GetChild(i)
 
-#check if file is ok for load
-if status == False:
-    print "WARNING: FbxImporter intialization failed"
-    #print importer.GetError()
-    sys.exit()
+        if child.GetNodeAttribute().GetAttributeType() == fbx.FbxNodeAttribute.eMesh:
+            #print("INFO: child = %s" % child)
+            mesh = child.GetNodeAttribute()
+            SVGline = DisplayControlsPoints(mesh)
 
+    # General file information
+    sceneInfo = scene.GetSceneInfo()
+    if sceneInfo:
+        #print("INFO: Title = %s" % sceneInfo.mTitle.Buffer())
+        #print("INFO: Subject = %s" % sceneInfo.mSubject.Buffer())
+        #print("INFO: Author = %s" % sceneInfo.mAuthor.Buffer())
+        #print("INFO: Keywords = %s" % sceneInfo.mKeywords.Buffer())
+        #print("INFO: Revision = %s" % sceneInfo.mRevision.Buffer())
+        #print("INFO: Comment = %s" % sceneInfo.mComment.Buffer())
 
-#import scene
-scene = fbx.FbxScene.Create(manager, "myScene")
-importer.Import(scene)
-importer.Destroy()
-
-
-#extract first node
-node = scene.GetRootNode()
-print("INFO: number of nodes = %i" % scene.GetNodeCount())
-print("INFO: number of geometries = %i" % scene.GetGeometryCount())
-
-for i in range(node.GetChildCount()):
-    print("INFO: number of child from node = %i" % node.GetChildCount())
-    child = node.GetChild(i)
-
-    if child.GetNodeAttribute().GetAttributeType() == fbx.FbxNodeAttribute.eMesh:
-        print("INFO: child = %s" % child)
-        mesh = child.GetNodeAttribute()
-        #i = lmesh.GetControlPointsCount()
-        SVGline = DisplayControlsPoints(mesh)
-
-
-# General file information
-sceneInfo = scene.GetSceneInfo()
-if sceneInfo:
-    print("INFO: Title = %s" % sceneInfo.mTitle.Buffer())
-    print("INFO: Subject = %s" % sceneInfo.mSubject.Buffer())
-    print("INFO: Author = %s" % sceneInfo.mAuthor.Buffer())
-    print("INFO: Keywords = %s" % sceneInfo.mKeywords.Buffer())
-    print("INFO: Revision = %s" % sceneInfo.mRevision.Buffer())
-    print("INFO: Comment = %s" % sceneInfo.mComment.Buffer())
-
-    thumbnail = sceneInfo.GetSceneThumbnail()
-    if thumbnail:
-        print("INFO: Thumbnail = yes")
+        thumbnail = sceneInfo.GetSceneThumbnail()
+        if thumbnail:
+            print("INFO: Thumbnail = yes")
+        else:
+            print("INFO: Thumbnail = no")
     else:
-        print("INFO: Thumbnail = no")
-else:
-    print ("WARNING: no model information")
+        print ("WARNING: no model information")
 
+    #render to image
+    DrawSVG(SVGline, SliceFileName(filePath))
+    return 1
 
-#render to image
+########################################
+#shows up in HTML page reading it from thumbnails directory
+########################################
+def CreateHTML():
+    thumbs = glob.glob('thumbnails/*.svg')
+    with document(title='Web Fbx Thumbnails') as doc:
+        h1('Thumbnails')
+        with table().add(tbody()):
+            lh = tr()
+            lh.add(td('Title', style="width:20%"))
+            lh.add(td('File Name', style="width:40%"))
+            lh.add(td('Image', style="width:40%"))
+            i = 0
+            for path in thumbs:
+                l = tr()
+                with l:
+                    l.add(td(SliceFileName(path)))
+                    l.add(td(path))
+                    l.add(td(img(src=path)))
+                i=i+1
 
-DrawSVG(SVGline, SliceFileName(tmp_file_path))
-DrawEmptySVG()
+    with open('gallery.html', 'w') as f:
+        f.write(doc.render())
 
+#temporary variables
+#path = "C:\Python27\Scripts\WebFbx"
+#/C:/Python27/Scripts/WebFbx/thumbnails/sample.svg
+#tmp_file_path = "files\cube.FBX";
+#tmp_file_path = "files\bat.fbx";
+#end temporary variables
+
+#process each FBX file in FILES directory
+found_files = glob.glob("files/*.fbx")
+for found_file in found_files:
+    print "%s" % found_file
+    ImportModel(found_file) #process each file in FBX SDK
 
 #shows up in HTML page reading it from thumbnails directory
-thumbs = glob.glob('thumbnails/*.svg')
-
-with document(title='Web Fbx Thumbnails') as doc:
-    h1('Thumbnails')
-    with table().add(tbody()):
-        lh = tr()
-        lh.add(td('Title', style="width:20%"))
-        lh.add(td('File Name', style="width:40%"))
-        lh.add(td('Image', style="width:40%"))
-        for path in thumbs:
-            l = tr()
-            with l:
-                #p(td('teste'), td(tmp_file_path), td(img(src=path)))
-                l.add(td('teste'))
-                l.add(td(tmp_file_path))
-                l.add(td(img(src=path)))
-
-with open('gallery.html', 'w') as f:
-    f.write(doc.render())
+CreateHTML()
